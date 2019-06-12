3ca97f21bdbb7cbfa86ecf8671e27f3c22f97023
ss-jscho28 <51615842+ss-jscho28@users.noreply.github.com>
2019-06-12 15:39:49 +0900

    Update Sort.java

--- Sort.java
+++ Sort.java
@@ -1,8 +1,9 @@
 import java.io.*;
 import java.lang.*;
+
 class Sort
 {
-	public static void main(String args[])throws IOException
+	public static void main(String args[]) throws IOException
 	{
 		int ch;
 		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
@@ -10,8 +11,7 @@ class Sort
 	{
 		System.out.println("\n\n\n1.Bubble Sort\n2.Selection Sort\n3.Insertion Sort.\n4.Quick Sort.\n5.Merge Sort.\n6.Exit.");
 		 ch=Integer.parseInt(br.readLine());
-		if(ch==6)
-			return;
+		if(ch==6) return;
 		System.out.println("Enter n");
 		int n=Integer.parseInt(br.readLine());
 		int a[]=new int[n];
@@ -47,21 +47,20 @@ class Sort
 	}
 	public static void BinarySort(int a[],int n)
 	{
-		int temp;
+		//int temp;
 		for(int i=0;i<n-1;i++)
 		{
 			for(int j=0;j<n-1;j++)
 			{
 				if(a[j]>a[(j+1)])
 				{
-					temp=a[j];
+					int temp=a[j];
 					a[j]=a[(j+1)];
 					a[(j+1)]=temp;
 				}
 			//System.out.print(a[j]);
 			}
-			//System.out.println();
-			
+			//System.out.println();			
 		}
 		print(a,n);
 	}
@@ -70,15 +69,16 @@ class Sort
 		for (int i=0;i<n-1;i++)
 		{
 			int imin=i;
-			int temp;
+			//int temp;
 			for(int j=i+1;j<n;j++)
 			{
 				if(a[j]<a[imin])
 					imin=j;
 			}
-					temp=a[i];
-					a[i]=a[imin];
-					a[imin]=temp;
+			
+			int temp=a[i];
+			a[i]=a[imin];
+			a[imin]=temp;
 		}
 		print(a,n);
 	}
@@ -100,15 +100,16 @@ class Sort
 	public static void MergeSort(int a[],int n)
 	{
 		
-		if(n<=1)
-			return;
+		if(n<=1) return;
 		int mid=n/2;
 		int left[]=new int[mid];
 		int right[]=new int[n-mid];
-		for(int i=0;i<mid;i++)
+		for(int i=0;i<mid;i++){
 			left[i]=a[i];
-		for(int i=mid;i<n;i++)
+		}
+		for(int i=mid;i<n;i++){
 			right[i-mid]=a[i];
+		}
 		MergeSort(left,mid);
 		MergeSort(right,n-mid);
 		Merge(left,right,a);
@@ -152,9 +153,9 @@ class Sort
 	{
 		if(start<end)
 		{
-		int pIndex=QuickPartition(a,start,end);
-		QuickSort(a,start,pIndex-1);
-		QuickSort(a,pIndex+1,end);
+			int pIndex=QuickPartition(a,start,end);
+			QuickSort(a,start,pIndex-1);
+			QuickSort(a,pIndex+1,end);
 		}
 		else
 			return;
@@ -170,21 +171,22 @@ class Sort
 			if(a[i]<=pivot)
 			{
 				//swap a[i],apindex
-					temp=a[i];
-					a[i]=a[pIndex];
-					a[pIndex]=temp;
-					pIndex++;
+				temp=a[i];
+				a[i]=a[pIndex];
+				a[pIndex]=temp;
+				pIndex++;
 			}
 		}
-					temp=a[pIndex];
-					a[pIndex]=a[end];
-					a[end]=temp;
-					return pIndex;
+		temp=a[pIndex];
+		a[pIndex]=a[end];
+		a[end]=temp;
+		return pIndex;
 	}
 	public static void print(int a[],int n)
 	{
 		System.out.println();
-		for(int i=0;i<n;i++)
+		for(int i=0;i<n;i++){
 			System.out.print(a[i]+"\t");
+		}
 	}
 }
